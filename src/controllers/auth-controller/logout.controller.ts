6ee63efb7d8b6279dcb641e731d79af2d3ba0c02
Alex Groß <alex@superschurke.de>
2023-12-25 10:43:47 +0100

    logout() to delete cookie with refresh token

--- src/controllers/auth-controller/logout.controller.ts
+++ src/controllers/auth-controller/logout.controller.ts
@@ -4,12 +4,12 @@ import {catchError} from "../generic-controller";
 export function logoutController(req: Request, res: Response) {
   try {
     // RefreshToken Cookie löschen
-    res.cookie(process.env.REFRESH_TOKEN_COOKIE || 'REFRESH_TOKEN_COOKIE', null,
+    res.cookie(process.env.REFRESH_TOKEN_COOKIE || 'REFRESH_TOKEN_COOKIE', '',
       {
         httpOnly: true,
         secure: true,
         sameSite: "none",
-        maxAge: 86_400_000,
+        maxAge: 0,
         path: "/"
       });
     return res.status(204).send()
