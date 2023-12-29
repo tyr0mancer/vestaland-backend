069f636849a09685792e674c2481a74fb707609b
Alex Groß <alex@superschurke.de>
2023-12-29 19:32:42 +0100

    gesamtDauer

--- src/models/rezept.model.ts
+++ src/models/rezept.model.ts
@@ -56,6 +56,9 @@ export class Rezept extends TimeStamps {
   @prop()
   public beschreibung?: string;
 
+  @prop()
+  public gesamtDauer?: number;
+
   @prop({ref: "Benutzer", type: mongoose.Schema.Types.ObjectId})
   public author?: Ref<Benutzer>;
 
