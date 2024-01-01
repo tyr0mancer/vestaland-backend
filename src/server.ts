9fab507082947e6465b3ad8b895f37b4ccaeb9de
Alex Groß <alex@superschurke.de>
2024-01-01 13:48:03 +0100

    console.log

--- src/server.ts
+++ src/server.ts
@@ -3,18 +3,14 @@ import {app} from "./app";
 
 const host = process.env.HOST || "localhost";
 const port = process.env.PORT || 3000;
+const db_host = process.env.DB_SERVER || "localhost";
 const start = async (): Promise<void> => {
   try {
-    const currentTime = new Date();
-    console.log(currentTime);
-    console.log('Versuche Verbindungsaufbau zu MongoDB')
-
-    // connect to mongoDB Atlas
+    console.log(new Date(), 'DB Verbindungsaufbau', db_host)
     const mongoUri = process.env.DB_CONNECTION_STRING || ""
     await mongoose.connect(mongoUri)
     app.listen(port, () => {
-      console.log('Erfolgreich mit MongoDB verbunden');
-      console.log(`Server läuft auf http://${host}:${port}`);
+      console.log(`Erfolgreich. Server läuft auf http://${host}:${port}`);
     });
   } catch (error) {
     console.error(error);
