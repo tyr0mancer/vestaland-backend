35cfaf52c2b9df25a6fc6a4662b12810e53a82a5
Alex Groß <alex@superschurke.de>
2024-01-11 21:33:44 +0100

    rezept.wartenErforderlich

--- src/shared-types/schema/Kochschritt.ts
+++ src/shared-types/schema/Kochschritt.ts
@@ -33,6 +33,9 @@ export class Kochschritt implements KochschrittType {
   public wartezeit?: number;
 
   @prop()
+  public wartenErforderlich?: boolean;
+
+  @prop()
   public resultatName?: string;
 
   @prop()
